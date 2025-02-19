5ab69dea8e52e93715faaa2058e2e970c0d22746
Bruno <b.cardoso@reply.com>
2025-02-18 22:10:48 -0300

    receber msg do sqs

--- crawler/main.ts
+++ crawler/main.ts
@@ -7,41 +7,39 @@ import { AdvertisementService } from './services/advertisement-service';
 
 export const handler = async (event) => {
     try {
-        console.log('Iniciando crawler...');
-        
-        console.log('Carregando configurações...');
-        // Carregar configurações do Secrets Manager
-        const configManager = ConfigManager.getInstance();
-        const config = await configManager.loadConfig();
-        console.log('Configurações carregadas com sucesso.');
+        for (const record of event.Records) {
+            console.log('Iniciando crawler...', JSON.stringify(event));
+            // Extrair dados da mensagem SQS
+            const messageBody = JSON.parse(record.body);
 
-        console.log('Iniciando serviços...');
-        // Inicializar serviços com as configurações
-        const uploader = new S3Uploader(config.aws);
-        const parser = new MLParser(uploader);
-        const scraper = new MLScraper(parser);
-        const adManager = new AdvertisementManager(config.database);
-        
-        const adService = new AdvertisementService(
-            scraper,
-            parser,
-            uploader,
-            adManager
-        );
-        console.log('Serviços iniciados com sucesso.');
+            console.log('Dados recebidos:', JSON.stringify(messageBody));
+            
+            console.log('Carregando configurações...');
+            // Carregar configurações do Secrets Manager
+            const configManager = ConfigManager.getInstance();
+            const config = await configManager.loadConfig();
+            console.log('Configurações carregadas com sucesso.');
 
-        console.log('Iniciando busca...');
-        // Executar a busca
-        await adService.searchAndSaveAdvertisements({
-            keyword: 'Máscara Nutricurls Wella 150g',
-            idBrand: '8eb1907c-0da7-4a37-8406-4f5066b77690',
-            brandProducts: [{
-                id_product: 'd67caf98-f6a6-456c-b3b2-0237d177a386',
-                st_varity_seq: '1',
-                st_varity_name: '100ML'
-            }]
-        });
-        console.log('Busca finalizada com sucesso.');
+            console.log('Iniciando serviços...');
+            // Inicializar serviços com as configurações
+            const uploader = new S3Uploader(config.aws);
+            const parser = new MLParser(uploader);
+            const scraper = new MLScraper(parser);
+            const adManager = new AdvertisementManager(config.database);
+            
+            const adService = new AdvertisementService(
+                scraper,
+                parser,
+                uploader,
+                adManager
+            );
+            console.log('Serviços iniciados com sucesso.');
+
+            console.log('Iniciando busca...');
+            // Executar a busca com os dados da mensagem SQS
+            await adService.searchAndSaveAdvertisements(messageBody);
+            console.log('Busca finalizada com sucesso.');
+        }
 
         return {
             statusCode: 200,
