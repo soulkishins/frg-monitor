09242a9de7604bbb4f86a13613ed0c38570ce81f
ricardomenezes-supply <ricardo.menezes@ext.supplymidia.com.br>
2025-03-04 16:25:52 -0300

    feature: add perfil/logout.

--- frontend/src/app/layout/component/app.topbar.ts
+++ frontend/src/app/layout/component/app.topbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
+import { CognitoService } from '../../pages/service/cognito.service';
 
 @Component({
     selector: 'app-topbar',
@@ -15,9 +16,22 @@ import { LayoutService } from '../service/layout.service';
 export class AppTopbar {
     items!: MenuItem[];
 
-    constructor(public layoutService: LayoutService) {}
+    constructor(
+        public layoutService: LayoutService,
+        private cognitoService: CognitoService,
+        private router: Router
+    ) {}
 
     toggleDarkMode() {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+    }
+
+    onProfile() {
+        this.router.navigate(['/cadastro/perfil']);
+    }
+
+    onLogout() {
+        this.cognitoService.logout();
+        this.router.navigate(['/auth/login']);
     }
 }
